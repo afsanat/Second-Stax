0ea0d1a6561d1060debc97fbf342fc2a2e1d40fa
Afsanat Ineza <afsanat.ineza@turntabl.io>
2023-03-22 13:51:43 +0000

    initial project

---
+++ Second-Stax/SecondStax/src/test/java/SecondStax/example/SecondStax/SecondStaxApplicationTests.java
@@ -0,0 +1,13 @@
+package SecondStax.example.SecondStax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondStaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
